9ada056a19620419b831d7f6284865a0a5522cf9
new121711 <alohikk@gmail.com>
2023-10-10 23:05:42 +0700

    READ DATA

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 // import { getFirestore } from "firebase/firestore";
-import { getFirestore, collection, doc, setDoc } from "firebase/firestore";
+import { getFirestore, collection, doc, setDoc, getDoc } from "firebase/firestore";
 
 
 import { getAnalytics } from "firebase/analytics";
@@ -24,30 +24,14 @@ const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 
 
-const citiesRef = collection(db, "cities");
+const docRef = doc(db, "cities", "AA");
 
-setDoc(doc(citiesRef, "SF"), {
-    name: "San Francisco", state: "CA", country: "USA",
-    capital: false, population: 860000,
-    regions: ["west_coast", "norcal"]
-});
-setDoc(doc(citiesRef, "LA"), {
-    name: "Los Angeles", state: "CA", country: "USA",
-    capital: false, population: 3900000,
-    regions: ["west_coast", "socal"]
-});
-setDoc(doc(citiesRef, "DC"), {
-    name: "Washington, D.C.", state: null, country: "USA",
-    capital: true, population: 680000,
-    regions: ["east_coast"]
-});
-setDoc(doc(citiesRef, "TOK"), {
-    name: "Tokyo", state: null, country: "Japan",
-    capital: true, population: 9000000,
-    regions: ["kanto", "honshu"]
-});
-setDoc(doc(citiesRef, "BJ"), {
-    name: "Beijing", state: null, country: "China",
-    capital: true, population: 21500000,
-    regions: ["jingjinji", "hebei"]
-});
+(async () => {
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+    } else {
+        // docSnap.data() will be undefined in this case
+        console.log("No such document!");
+    }
+})()
